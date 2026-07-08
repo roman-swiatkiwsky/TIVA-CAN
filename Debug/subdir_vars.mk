f0d6f9bba1346473ce2771eb0cbe21b61ecbc232
romanswi <romanswi@buffalo.edu>
2026-07-08 10:21:14 -0400

    first commit

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,39 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+C_SRCS += \
+../lib_c.c \
+../main.c \
+../tm4c123gh6pm_startup_ccs.c 
+
+C_DEPS += \
+./lib_c.d \
+./main.d \
+./tm4c123gh6pm_startup_ccs.d 
+
+OBJS += \
+./lib_c.obj \
+./main.obj \
+./tm4c123gh6pm_startup_ccs.obj 
+
+OBJS__QUOTED += \
+"lib_c.obj" \
+"main.obj" \
+"tm4c123gh6pm_startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"lib_c.d" \
+"main.d" \
+"tm4c123gh6pm_startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../lib_c.c" \
+"../main.c" \
+"../tm4c123gh6pm_startup_ccs.c" 
+
+
